00c431235f6bc281746a99dd4854c2e1036ecce9
rajivsharma92 <rajiv8982@gmail.com>
2024-09-03 21:29:23 +0530

    circle area feature added

--- calculator_plus_app.py
+++ calculator_plus_app.py
@@ -1,44 +1,28 @@
+import math
 
-class Calculator:
+class GeometryCalculator:
 
-    def add(self, a, b):
+    def calculate_circle_area(self, radius):
 
-        return a + b
+        return math.pi * radius ** 2
 
-    def subtract(self, a, b):
+    def calculate_rectangle_area(self, length, width):
 
-        return a - b
-
-    def multiply(self, a, b):
-
-        return a * b
-
-    def divide(self, a, b):
-        if b == 0:
-            raise ValueError(" Cannot divide by zero. ")
-        return a / b
-
-    def square_root(self, x):
-
-        return math.sqrt(x)
-
+        return length * width
 
 if __name__ == "__main__":
 
-    calculator = Calculator()
+    calculator = GeometryCalculator()
 
-    num1 = 16
+# TODO: Implement the feature to calculate the area of a circle
 
-    num2 = 4
+    radius = 5
 
-    print(f"{num1} + {num2} = {calculator.add(num1, num2)}")
+    print(f"The area of the circle with radius {radius} = \
+          {calculator.calculate_circle_area(radius)}")
 
-    print(f"{num1} - {num2} = {calculator.subtract(num1, num2)}") 
+# TODO: Implement the feature to calculate the area of a rectangle # length = 10
 
-    print(f"{num1} * {num2} = {calculator.multiply(num1, num2)}")
+# width = 6
 
-    print(f"{num1} / {num2} = {calculator.divide(num1, num2)}")
-
-    num3 = 25
-
-    print(f"The square root of {num3} = {calculator.square_root(num3)}")
+# print(f"The area of the rectangle with length {length} and width {width} = {calculator.calculate_rectangle_area(length, width)}")
