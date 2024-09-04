204f3fbf27b9a1fa5379af744118bd748c167229
Rajiv Sharma <rajiv8982@gmail.com>
2024-09-04 13:49:44 +0530

    Update calculator_plus_app.py

--- calculator_plus_app.py
+++ calculator_plus_app.py
@@ -2,16 +2,10 @@ import math
 
 class GeometryCalculator:
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
     def calculate_circle_area(self, radius):
 
         return math.pi * radius ** 2
 
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
     def calculate_rectangle_area(self, length, width):
 
         return length * width
@@ -20,37 +14,16 @@ if __name__ == "__main__":
 
     calculator = GeometryCalculator()
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 # TODO: Implement the feature to calculate the area of a circle
 
     radius = 5
 
     print(f"The area of the circle with radius {radius} = \
           {calculator.calculate_circle_area(radius)}")
-
-# TODO: Implement the feature to calculate the area of a rectangle # length = 10
-
-# width = 6
-
-# print(f"The area of the rectangle with length {length} and width {width} = {calculator.calculate_rectangle_area(length, width)}")
-=======
-# Implement the feature to calculate the area of a rectangle 
  
-length = 10
+    length = 10
 
-width = 6
+    width = 6
 
-print(f"The area of the rectangle with length {length} and width {width} = \
+    print(f"The area of the rectangle with length {length} and width {width} = \
       {calculator.calculate_rectangle_area(length, width)}")
->>>>>>> Stashed changes
-=======
-# Implement the feature to calculate the area of a rectangle 
- 
-length = 10
-
-width = 6
-
-print(f"The area of the rectangle with length {length} and width {width} = \
-      {calculator.calculate_rectangle_area(length, width)}")
->>>>>>> Stashed changes
